14bc53da0175cfc0616cf7ae9504cf13d0e041b4
Utsav Bhavsar <utsavb96@gmail.com>
2018-09-24 18:44:18 +0530

    allow access to all domains with all http methods

--- models/todo.go
+++ models/todo.go
@@ -1,14 +1,18 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
+
 	"github.com/utsav/gin-gonic-boilerplate/db"
 )
 
 type Todo struct {
-	gorm.Model
-	Title       string `json:"title" binding:"required"`
-	Description string `json:"description" binding:"required"`
+	ID          uint       `json:"id" gorm:"primary_key"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at" sql:"index"`
+	Title       string     `json:"title" binding:"required"`
+	Description string     `json:"description" binding:"required"`
 }
 
 func init() {
--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,26 @@
 package routes
 
-import "github.com/gin-gonic/gin"
-import ctrl "github.com/utsav/gin-gonic-boilerplate/controllers"
+import (
+	"github.com/gin-gonic/gin"
+	ctrl "github.com/utsav/gin-gonic-boilerplate/controllers"
+)
 
 func Run() {
 	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		// add header Access-Control-Allow-Origin
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Add("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Add("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
+		c.Writer.Header().Add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	})
+
 	r.GET("/todos", ctrl.GetTodos)
 	r.GET("/todo/:id", ctrl.GetTodo)
 	r.POST("/todo", ctrl.CreateTodo)
